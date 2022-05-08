e2e87ec9d32ee9b883beaf649cef7ddb95477c06
soni240 <s.fegade21@gmail.com>
2022-05-08 23:07:46 +0530

    string no

--- Program.cs
+++ Program.cs
@@ -3,27 +3,34 @@ namespace FindMaximumNoProject
 {
     class MaximumNumberCheck
     {
-        public static double MaximumFloatNumber(double firstValue, double secondValue, double thirdValue)
+        public static string MaximumStringNumber(string firstString, string secondString, string thirdString)
         {
-            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+            if (firstString.CompareTo(secondString) > 0 && firstString.CompareTo(thirdString) > 0 ||
+                firstString.CompareTo(secondString) >= 0 && firstString.CompareTo(thirdString) > 0 ||
+                firstString.CompareTo(secondString) > 0 && firstString.CompareTo(thirdString) >= 0)
             {
-                return firstValue;
+                return firstString;
             }
-            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+
+            if (secondString.CompareTo(firstString) > 0 && secondString.CompareTo(thirdString) > 0 ||
+                secondString.CompareTo(firstString) >= 0 && secondString.CompareTo(thirdString) > 0 ||
+                secondString.CompareTo(firstString) > 0 && secondString.CompareTo(thirdString) >= 0)
             {
-                return secondValue;
+                return secondString;
             }
-            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0)
+
+            if (thirdString.CompareTo(firstString) > 0 && thirdString.CompareTo(secondString) > 0 ||
+                thirdString.CompareTo(firstString) >= 0 && thirdString.CompareTo(secondString) > 0 ||
+                thirdString.CompareTo(firstString) > 0 && thirdString.CompareTo(secondString) >= 0)
             {
-                return thirdValue;
+                return thirdString;
             }
             throw new Exception("firstNumber,secondNumber and thirdNumber are same");
         }
     }
 }
-
-
-
-
-
+           public override string? ToString()
+           {         
+              return base.ToString();
+           }
 
