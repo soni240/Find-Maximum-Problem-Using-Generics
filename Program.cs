fe8307919f1f6d5546f6706855c867bf52ad23db
soni240 <s.fegade21@gmail.com>
2022-05-08 23:01:12 +0530

    maximum no

--- Program.cs
+++ Program.cs
@@ -1,37 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
-//Console.WriteLine("Hello, World!");
-using Array_Sample_Code;
-using System;
-
-internal class ArrayInput
+namespace FindMaximumNoProject
 {
-    public static void toPrint<X>(X[] inputArray)
+    class MaximumNumberCheck
     {
-        foreach (var element in inputArray)
+        public static int MaximumIntegerNumber(int firstValue, int secondValue, int thirdValue)
         {
-            Console.WriteLine(element);
-        }
-        //for (int i = 0; i < inputArray.Length; i = i + 2)
-        //{
-        //    Console.WriteLine(inputArray[i]);
-        //}
-        Console.WriteLine("----------------------------------");
-    }
-    class program
-    {
-        static void Main(string[] args)
-        {
-            int[] intArray = { 1, 2, 3, 4, 5 };
-            double[] doubleArray = { 1.1, 2.2, 3.3, 4.4 };
-            char[] charArray = { 'H', 'E', 'L', 'L', 'O' };
-            //Program.toPrint(intArray);
-            //Program.toPrint(doubleArray);
-            //Program.toPrint(charArray);
-
-            ArrayInput.toPrint<int>(intArray);
-            ArrayInput.toPrint<double>(doubleArray);
-            ArrayInput.toPrint<char>(charArray);
-            Console.ReadKey();
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+            {
+                return firstValue;
+            }
+            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+            {
+                return secondValue;
+            }
+            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0)
+            {
+                return thirdValue;
+            }
+            throw new Exception("firstNumber,secondNumber and thirdNumber are same");
         }
     }
 }
